9da836812d8b13895fa8aad627aa069e08150e6e
RawToast <jim@coursebase.co>
2020-11-23 23:01:40 +0900

    movement

--- Player.gd
+++ Player.gd
@@ -1,28 +1,48 @@
 extends Area2D
+
+# declare a signal (observer)
+signal hit
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 export (int) var speed  # How fast the player will move (pixels/sec).
-var screensize  # Size of the game window.
+var screen_size  # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screensize = get_viewport_rect().size
+  screen_size = get_viewport_rect().size
+  # hide the player
+  hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	var velocity = Vector2() # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-			velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-			velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-			velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-			velocity.y -= 1
-	if velocity.length() > 0:
-			velocity = velocity.normalized() * speed
-			$AnimatedSprite.play()
-	else:
-			$AnimatedSprite.stop()
+func _process(delta):
+  var velocity = Vector2() # The player's movement vector.
+  if Input.is_action_pressed("ui_right"):
+    velocity.x += 1
+  if Input.is_action_pressed("ui_left"):
+    velocity.x -= 1
+  if Input.is_action_pressed("ui_down"):
+    velocity.y += 1
+  if Input.is_action_pressed("ui_up"):
+    velocity.y -= 1
+  if velocity.length() > 0:
+    velocity = velocity.normalized() * speed
+    $AnimatedSprite.play()
+  else:
+    $AnimatedSprite.stop()
+
+  position += velocity * delta
+  position.x = clamp(position.x, 0, screen_size.x)
+  position.y = clamp(position.y, 0, screen_size.y)
+
+  if velocity.x != 0:
+    $AnimatedSprite.animation = "walk"
+    $AnimatedSprite.flip_v = false
+    # See the note below about boolean assignment
+    $AnimatedSprite.flip_h = velocity.x < 0
+  elif velocity.y != 0:
+    $AnimatedSprite.animation = "up"
+    $AnimatedSprite.flip_v = velocity.y > 0
+      
