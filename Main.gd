4f58b638d0f9338a87df9df28d6809326297841a
RawToast <jmorris@itazuramono.com>
2020-12-23 17:35:05 +0000

    Working HUD

--- Main.gd
+++ Main.gd
@@ -5,7 +5,7 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
+	# new_game()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -13,16 +13,19 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
-
-func game_over():
+func _game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_MobTimer_timeout():
 	# Choose a random location on Path2D.
